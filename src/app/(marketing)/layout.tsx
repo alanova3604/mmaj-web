98642cebb22b054e1692f7376980cdff6d7646d4
Alan Valdez <orlando@nubir.mx>
2026-03-31 13:46:42 -0600

    feat: add Microsoft Clarity analytics tracking

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css"
 import Header from "@/componentes/layout/Header";
+import Script from 'next/script'
 import Footer from "@/componentes/layout/Footer";
 import MotionWrapper from "@/componentes/animation/MotionWrapper";
 
@@ -48,7 +49,19 @@ export default function RootLayout({ children, }: Readonly<{
           {children}
           <Footer></Footer>
         </div>
+
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+          (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ttvz06xfes");
+          `}
+        </Script>
       </body>
+
+
     </html>
   );
 }
